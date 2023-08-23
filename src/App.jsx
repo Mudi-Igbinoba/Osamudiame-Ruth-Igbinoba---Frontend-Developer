119e3fa7ded22fbc7aa8bf668bf0203a1a91557c
Osamudiame Ruth Igbinoba <ruth.igbinoba12@gmail.com>
2023-08-23 13:00:47 +0100

    Initial commit
    Configured Vite React App and TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Hero from './components/Hero';
+
+function App() {
+    return (
+        <div className='font-robotoSlab'>
+            <Hero />
+            <main></main>
+        </div>
+    );
+}
+
+export default App;
