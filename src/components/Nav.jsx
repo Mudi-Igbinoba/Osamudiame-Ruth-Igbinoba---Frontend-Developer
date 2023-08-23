e8b9c4991823305b74b57666e5f222612a65c854
Osamudiame Ruth Igbinoba <ruth.igbinoba12@gmail.com>
2023-08-23 13:02:56 +0100

    Create Nav.jsx
    created navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import Logo from '../assets/img/logo.png';
+
+const Nav = () => {
+    return (
+        <nav className='xl:py-8 sm:py-5 py-4 sticky top-0'>
+            <ul className=''>
+                <li>
+                    <a href='/'>
+                        <img
+                            src={Logo}
+                            alt='SpaceX Logo'
+                            className='sm:w-1/4 w-2/5 mx-auto'
+                        />
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
