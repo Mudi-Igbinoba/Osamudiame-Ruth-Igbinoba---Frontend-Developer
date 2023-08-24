c7adf33d623213ac201a2df6f39de8275e051f0b
Osamudiame Ruth Igbinoba <ruth.igbinoba12@gmail.com>
2023-08-24 22:41:03 +0100

    Update SearchForm.jsx
    restructured code and added useEffect

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,10 +1,52 @@
 import { HiOutlineStatusOnline, HiOutlineClock } from 'react-icons/hi';
 import { VscTypeHierarchySub } from 'react-icons/vsc';
+import { useSearchContext } from './SearchContext';
+import { useState, useEffect } from 'react';
 
-const SearchForm = () => {
+const SearchForm = ({ setData }) => {
+    const { searchFilters, setSearchFilters } = useSearchContext();
+
+    const [formData, setFormData] = useState({
+        status: searchFilters.status,
+        landings: searchFilters.landings,
+        type: searchFilters.type,
+    });
+
+    const handleFormChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setSearchFilters(formData); // Update the context on form submit
+    };
+
+    useEffect(() => {
+        // Construct the query string based on selected filters
+        const queryString = `https://api.spacexdata.com/v3/capsules?status=${searchFilters.status}&landings=${searchFilters.landings}&type=${searchFilters.type}`;
+
+        // Fetch data from SpaceX API
+        fetch(queryString)
+            .then((response) => response.json())
+            .then((data) => {
+                // Handle fetched data
+                setData(data);
+            })
+            .catch((error) => {
+                // Handle errors
+                console.error(error);
+            });
+    }, [searchFilters, setData]);
+
     return (
         <section className='py-5'>
-            <form className='text-center grid gap-4 sm:grid-cols-2 sm:grid-rows-2 sm:text-base text-sm'>
+            <form
+                className='text-center grid gap-4 sm:grid-cols-2 sm:grid-rows-2 sm:text-base text-sm'
+                onSubmit={handleSubmit}>
                 <div>
                     <label
                         htmlFor='status'
@@ -16,8 +58,10 @@ const SearchForm = () => {
                     <select
                         className='mt-2.5 text-black py-2 px-3 rounded-lg'
                         name='status'
-                        id='status'>
-                        <option value='' disabled selected>
+                        onChange={handleFormChange}
+                        id='status'
+                        value={formData.status}>
+                        <option value='' disabled>
                             Select Status
                         </option>
                         <option value='active'>Active</option>
@@ -29,29 +73,25 @@ const SearchForm = () => {
 
                 <div>
                     <label
-                        htmlFor='launch'
+                        htmlFor='landings'
                         className='uppercase tracking-widest'>
-                        Original Launch
+                        Landings
                         <HiOutlineClock className='inline ml-2 mb-1 text-white animate-spin' />
                     </label>
                     <br />
                     <select
                         className='mt-2.5 text-black py-2 px-3 rounded-lg'
-                        name='launch'
-                        id='launch'>
-                        <option value='' disabled selected>
-                            Select a year
+                        name='landings'
+                        onChange={handleFormChange}
+                        id='landings'
+                        value={formData.landings}>
+                        <option value='' disabled>
+                            Select Landings
                         </option>
-                        <option value='2010'>2010</option>
-                        <option value='2012'>2012</option>
-                        <option value='2013'>2013</option>
-                        <option value='2014'>2014</option>
-                        <option value='2015'>2015</option>
-                        <option value='2016'>2016</option>
-                        <option value='2017'>2017</option>
-                        <option value='2019'>2019</option>
-                        <option value='2020'>2020</option>
-                        <option value='null'>Null</option>
+                        <option value={0}>0</option>
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
                     </select>
                 </div>
 
@@ -64,9 +104,11 @@ const SearchForm = () => {
                     <select
                         className='mt-2.5 text-black py-2 px-3 rounded-lg'
                         name='type'
-                        id='type'>
-                        <option value='' disabled selected>
-                            Select a type
+                        onChange={handleFormChange}
+                        id='type'
+                        value={formData.type}>
+                        <option value='' disabled>
+                            Select Type
                         </option>
                         <option value='Dragon 1.0'>Dragon 1.0</option>
                         <option value='Dragon 1.1'>Dragon 1.1</option>
