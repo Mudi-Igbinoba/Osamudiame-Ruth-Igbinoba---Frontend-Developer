e2097a73f304a1d0e51445c6f5b9cebc0d158dc8
Osamudiame Ruth Igbinoba <ruth.igbinoba12@gmail.com>
2023-08-25 19:32:20 +0100

    Update SearchContext.jsx

--- src/components/SearchContext.jsx
+++ src/components/SearchContext.jsx
@@ -2,10 +2,12 @@ import { createContext, useContext, useState } from 'react';
 
 const SearchContext = createContext();
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useSearchContext = () => {
     return useContext(SearchContext);
 };
 
+// eslint-disable-next-line react/prop-types
 export const SearchProvider = ({ children }) => {
     const [searchFilters, setSearchFilters] = useState({
         status: '',
